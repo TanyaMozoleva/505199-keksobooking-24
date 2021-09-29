1b4ad6630c38464c1cc34139cb57fcea4c2b4869
TanyaMozoleva <37499067+TanyaMozoleva@users.noreply.github.com>
2021-09-29 22:14:26 +0300

    fixing mistakes

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 const getRandomInt = function(min, max) {
-  if (min >=0 && min < max) {
+  if (min >= 0 && min < max) {
     const rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
   }
-  return ('Переданный диапазон чисел введен некорректно');
+  throw new Error('Переданный диапазон чисел введен некорректно');
 };
 
 getRandomInt(20,30);
 
 const getRandomFloat = function(min, max,accuracy) {
-  if (min >=0 && min < max) {
+  if (min >= 0 && min < max) {
     const floatNumber = Math.random() *  (max - min) + min;
-    return +floatNumber.toFixed(accuracy);
+    return Number(floatNumber.toFixed(accuracy));
   }
-  return ('Переданный диапазон чисел введен некорректно');
+  throw new Error('Переданный диапазон чисел введен некорректно');
 };
 
 getRandomFloat(20.5, 35.5, 6);
